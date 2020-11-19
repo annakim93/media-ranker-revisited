76d41efc03e1854e323d129d3ed9e4b610e7b141
Anna Kim <anna.kim93@gmail.com>
2020-11-19 00:39:45 -0500

    revise find_user method in application controller to redirect and flash error message when not logged in; skip find_user for root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :find_user
 
   def render_404
@@ -12,6 +11,9 @@ class ApplicationController < ActionController::Base
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
+    else
+      flash[:error] = 'Please log in to perform this action.'
+      redirect_to root_path
     end
   end
 end
