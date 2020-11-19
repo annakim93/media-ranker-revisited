4268ea10cb5523d7db20150f61403d20ab3bf35d
Anna Kim <anna.kim93@gmail.com>
2020-11-18 23:12:45 -0500

    fix perform_login to include user.find_by

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,17 +7,27 @@ describe UsersController do
   describe 'login' do
     it "logs in an existing user and redirects to the root route" do
       start_count = User.count
-      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+      perform_login(user)
 
-      get omniauth_callback_path(:github)
       must_redirect_to root_path
-
       expect(session[:user_id]).must_equal user.id
-
       expect(User.count).must_equal start_count
     end
 
     it "creates an account for a new user and redirects to the root route" do
+      new_user = User.new(
+        username: 'username',
+        provider: 'github',
+        email: 'someone@somewhere.com',
+        uid: 123
+      )
+
+      expect{
+        perform_login(new_user)
+      }.must_change "User.count", 1
+
+      must_redirect_to root_path
+      expect(session[:user_id]).must_equal User.last.id
     end
 
     it "redirects to the login route if given invalid user data" do
