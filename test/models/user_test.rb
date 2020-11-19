43a1b1c25909abf3590b22eac53c45bc85ae0d98
Anna Kim <anna.kim93@gmail.com>
2020-11-19 01:44:14 -0500

    add test for custom method build_from_github

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,6 +2,16 @@ require "test_helper"
 
 describe User do
   let (:user) { User.first }
+  let (:auth_hash) {
+    {
+        provider: 'github',
+        uid: 123456,
+        info: {
+            email: 'test@test.com',
+            name: 'testtest'
+        }
+    }
+  }
 
   describe "relations" do
     it "has a list of votes" do
@@ -67,6 +77,17 @@ describe User do
       expect(user.valid?).must_equal false
       expect(user.errors.messages).must_include :email
       expect(user.errors.messages[:email]).must_equal ['has already been taken']
+    end
+  end
+
+  describe 'custom methods' do
+    it 'build_from_github: returns a user given a hash' do
+      new_user = User.build_from_github(auth_hash)
+
+      expect(new_user.username).must_equal auth_hash[:info][:name]
+      expect(new_user.email).must_equal auth_hash[:info][:email]
+      expect(new_user.uid).must_equal auth_hash[:uid]
+      expect(new_user.provider).must_equal auth_hash[:provider]
     end
   end
 end
