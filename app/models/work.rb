b3ab9f409f287fe18c18fa9e60091827aa95e181
Anna Kim <anna.kim93@gmail.com>
2020-11-30 15:42:53 -0500

    migration to add relationship work belongs to user who created it

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,6 +2,7 @@ class Work < ApplicationRecord
   CATEGORIES = %w(album book movie)
   has_many :votes, dependent: :destroy
   has_many :ranking_users, through: :votes, source: :user
+  belongs_to :user # choosing to leave work if user account is destroyed - still valid to be voted on
 
   validates :category, presence: true,
                        inclusion: {in: CATEGORIES}
